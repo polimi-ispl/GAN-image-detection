6bd873538548cb6e5fab2c96fb4ad37b50def851
Sara Mandelli <smandell91@gmail.com>
2021-10-06 15:26:07 +0200

    Update detector

--- gan_vs_real_detector.py
+++ gan_vs_real_detector.py
@@ -11,39 +11,14 @@ torch.multiprocessing.set_sharing_strategy('file_system')
 import albumentations as A
 import albumentations.pytorch as Ap
 from utils import architectures
+from utils.python_patch_extractor.PatchExtractor import PatchExtractor
 from PIL import Image
 
 
 class Detector:
     def __init__(self):
 
-        # model directory and path for detector A
-        # model_A_dir = 'weights/method_A/net-EfficientNetB4_lr-0.001_img_aug-[\'flip\', \'rotate\', \'clahe\', \'blur\', ' \
-        #               '\'brightness&contrast\', \'jitter\', \'downscale\', \'hsv\', \'resize\', \'jpeg\']' \
-        #               '_img_aug_p-0.5_patch_size-128_patch_number-1_batch_size-250_num_classes-2'
-        #
-        # # model directory and path for detector B
-        # model_B_dir = 'weights/method_B/net-EfficientNetB4_lr-0.001_aug-[\'flip\', \'rotate\', \'clahe\', \'blur\', ' \
-        #               '\'crop&resize\', \'brightness&contrast\', \'jitter\', \'downscale\', \'hsv\']' \
-        #               '_aug_p-0.5_jpeg_aug_p-0.7_patch_size-128_patch_number-1_batch_size-250_num_classes-2'
-        #
-        # # model directory and path for detector C
-        # model_C_dir = 'weights/method_C/net-EfficientNetB4_lr-0.001_aug-[\'flip\', \'rotate\', \'clahe\', \'blur\',' \
-        #               ' \'crop&resize\', \'brightness&contrast\', \'jitter\', \'downscale\', \'hsv\']' \
-        #               '_aug_p-0.5_jpeg_aug_p-0_patch_size-128_patch_number-5_batch_size-50_num_classes-2'
-        #
-        # # model directory and path for detector D
-        # model_D_dir = 'weights/method_D/net-EfficientNetB4_lr-0.001_aug-[\'flip\', \'rotate\', \'clahe\', \'blur\',' \
-        #               '\'crop&resize\', \'brightness&contrast\', \'jitter\', \'downscale\', \'hsv\']' \
-        #               '_aug_p-0.5_jpeg_aug_p-0_patch_size-128_patch_number-10_batch_size-25_num_classes-2'
-        #
-        # # model directory for detector E
-        # model_E_dir = 'weights/method_E/net-EfficientNetB4_lr-0.001_aug-[\'flip\', \'rotate\', \'clahe\', \'blur\',' \
-        #               ' \'crop&resize\', \'brightness&contrast\', \'jitter\', \'downscale\', \'hsv\']' \
-        #               '_aug_p-0.5_jpeg_aug_p-0.7_patch_size-128_patch_number-1_batch_size-250_num_classes-2'
-
-        self.weights_path_list = [os.path.join('weights', f'method_{x}.pth') for x in 'ABCDE']
-        # self.model_path = os.path.join(model_dir, 'bestval.pth')
+        self.weights_path_list = [os.path.join('/nas/home/nbonettini/projects/StyleGAN3-detection/weights', f'method_{x}.pth') for x in 'ABCDE']
 
         # GPU configuration if available
         self.device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
@@ -72,17 +47,15 @@ class Detector:
             Ap.transforms.ToTensorV2()
         ]
         self.trans = A.Compose(transform)
-
         self.cropper = A.RandomCrop(width=128, height=128, always_apply=True, p=1.)
-
         self.criterion = torch.nn.CrossEntropyLoss(reduction='none')
 
-    def synth_real_detector(self, img_path: str, n_patch: int = 50):
+    def synth_real_detector(self, img_path: str, n_patch: int = 200):
 
         # Load image:
         img = np.asarray(Image.open(img_path))
 
-        # Optout if image is non conforming
+        # Opt-out if image is non conforming
         if img.shape == ():
             print('{} None dimension'.format(img_path))
             return None
@@ -96,47 +69,52 @@ class Detector:
             print('Omitting alpha channel')
             img = img[:, :, :3]
 
-        # Extract test_N random patches from image:
-        patch_list = [self.cropper(image=img)['image'] for _ in range(n_patch)]
+        img_net_scores = []
+        for net_idx, net in enumerate(self.nets):
 
-        # Normalization
-        transf_patch_list = [self.trans(image=patch)['image'] for patch in patch_list]
+            if net_idx == 0:
 
-        # Compute scores
-        transf_patch_tensor = torch.stack(transf_patch_list, dim=0).to(self.device)
-        with torch.no_grad():
-            patch_scores = self.net(transf_patch_tensor)
-        softmax_scores = torch.softmax(patch_scores, dim=1)
-        predictions = torch.argmax(softmax_scores, dim=1)
+                # only for detector A, extract N = 200 random patches per image
+                patch_list = [self.cropper(image=img)['image'] for _ in range(n_patch)]
 
-        # Majority voting on patches
-        if sum(predictions) > len(predictions) // 2:
-            majority_voting = 1
-        else:
-            majority_voting = 0
+            else:
 
-        # get an output score from softmax scores:
-        # LLR < 0: real
-        # LLR > 0: synthetic
+                # for detectors B, C, D, E, extract patches aligned with the 8 x 8 pixel grid:
+                # we want more or less 200 patches per img
+                stride_0 = ((((img.shape[0] - 128) // 20) + 7) // 8) * 8
+                stride_1 = (((img.shape[1] - 128) // 10 + 7) // 8) * 8
+                pe = PatchExtractor(dim=(128, 128, 3), stride=(stride_0, stride_1, 3))
+                patches = pe.extract(img)
+                patch_list = list(patches.reshape((patches.shape[0]*patches.shape[1], 128, 128, 3)))
 
-        sign_predictions = majority_voting * 2 - 1
-        # select only the scores associated with the estimated class (by majority voting)
-        softmax_scores = softmax_scores[:, majority_voting]
-        normalized_prediction = torch.max(softmax_scores).item() * sign_predictions
+            # Normalization
+            transf_patch_list = [self.trans(image=patch)['image'] for patch in patch_list]
 
-        return normalized_prediction
+            # Compute scores
+            transf_patch_tensor = torch.stack(transf_patch_list, dim=0).to(self.device)
+            with torch.no_grad():
+                patch_scores = net(transf_patch_tensor).cpu().numpy()
+                patch_predictions = np.argmax(patch_scores, axis=1)
+
+            maj_voting = np.any(patch_predictions).astype(int)
+            scores_maj_voting = patch_scores[:, maj_voting]
+            img_net_scores.append(np.nanmax(scores_maj_voting) if maj_voting == 1 else -np.nanmax(scores_maj_voting))
+
+        # final score is the average among the 5 scores returned by the detectors
+        img_score = np.mean(img_net_scores)
+
+        return img_score
 
 
 def main():
-    # img_path
-    img_path = "/nas/public/exchange/semafor/eval1/stylegan2/100k-generated-images/car-512x384_cropped/stylegan2-" \
-               "config-f-psi-0.5/097000/097001.png"
 
-    # number of random patches to extract from images
-    test_N = 50
+    # img_path on fermi:
+    img_path = '/home/nbonettini/nvidia_temp/nvidia-alias-free-gan/faces/alias-free-r-afhqv2-512x512/seed40000.png'
 
     detector = Detector()
-    detector.synth_real_detector(img_path, test_N)
+    score = detector.synth_real_detector(img_path)
+
+    print('Image Score: {}'.format(score))
 
     return 0
 
